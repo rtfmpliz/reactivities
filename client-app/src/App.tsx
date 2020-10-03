35cb72fa87ff38dba410f7df4460301d037d24b0
wahyu <wahyu@istw.co.id>
2020-10-03 18:00:04 +0700

    10 tapi logo gak keluar

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {Component} from "react";
+import logo from './logo.svg';
 import "./App.css";
-import { cars } from "./demo";
-import { CarItem } from "./CarItem";
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map((car) => (
-          <CarItem car={car}/>
-        ))}
-      </ul>
-    </div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="{logo}" className='App-logo' alt=""/>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload
+          </p>
+          <a href="https://reactjs.org"
+          className='App-link'
+          target='_blank'
+          rel='noopener noreferrer'
+          >
+            Learn REact
+          </a>
+        </header>
+      </div>
   );
+  }
 }
 
 export default App;
