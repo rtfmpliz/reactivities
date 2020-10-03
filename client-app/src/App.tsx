87b0f900e2a5b063bb1f3c0a4d8626e2d71311cc
wahyu <wahyu@istw.co.id>
2020-10-03 18:53:03 +0700

    11

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,22 +2,28 @@ import React, {Component} from "react";
 import logo from './logo.svg';
 import "./App.css";
 
+
 class App extends Component {
+  state = {
+    values : []
+  }
+
+  componentDidMount(){
+    this.setState({
+      values: [{id:1, name: "Value 101"},{id:2,name:"Values 102"}]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="{logo}" className='App-logo' alt=""/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload
-          </p>
-          <a href="https://reactjs.org"
-          className='App-link'
-          target='_blank'
-          rel='noopener noreferrer'
-          >
-            Learn REact
-          </a>
+          <ul>
+
+          {this.state.values.map((value: any)=>(
+            <li>{value.name}</li>
+          ))}
+          </ul>
         </header>
       </div>
   );
