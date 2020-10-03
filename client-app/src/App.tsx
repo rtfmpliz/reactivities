26be4a342c3e058669a7930fbbfd0c423f8ff3db
wahyu <wahyu@istw.co.id>
2020-10-03 19:25:08 +0700

    12

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import logo from './logo.svg';
 import "./App.css";
+import axios from 'axios';
 
 
 class App extends Component {
@@ -9,9 +10,14 @@ class App extends Component {
   }
 
   componentDidMount(){
-    this.setState({
-      values: [{id:1, name: "Value 101"},{id:2,name:"Values 102"}]
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      })
     })
+ 
   }
 
   render() {
