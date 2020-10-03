86de4f19232ea593ef2c107d9bbf8af94aaff080
wahyu <wahyu@istw.co.id>
2020-10-03 21:56:43 +0700

    14

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,38 +1,38 @@
-import React, {Component} from "react";
-import logo from './logo.svg';
+import React, { Component } from "react";
 import "./App.css";
-import axios from 'axios';
-
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends Component {
   state = {
-    values : []
-  }
+    values: [],
+  };
 
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
       console.log(response);
       this.setState({
-        values: response.data
-      })
-    })
- 
+        values: response.data,
+      });
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <ul>
-
-          {this.state.values.map((value: any)=>(
-            <li key={value.id}>{value.name}</li>
+      <div>
+        <Header as="h2">
+          <Icon name="plug" />
+          <Header.Content>Uptime Guarantee</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}> {value.name}</List.Item>
           ))}
-          </ul>
-        </header>
+
+        </List>
+
       </div>
-  );
+    );
   }
 }
 
