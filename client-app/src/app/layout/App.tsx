7f3e89de7c6487ca73a80eccb9d5fe804570e89a
wahyu <wahyu@istw.co.id>
2020-11-15 15:33:11 +0700

    5.5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,13 +3,19 @@ import "./styles.css";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
 
-class App extends Component {
-  state = {
+import {IActivity} from '../models/activity'
+
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  readonly state : IState= {
     activities: [],
   };
 
   componentDidMount() {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
+    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
       this.setState({
         activities: response.data,
       });
@@ -24,7 +30,7 @@ class App extends Component {
           <Header.Content>Uptime Guarantee</Header.Content>
         </Header>
         <List>
-          {this.state.activities.map((activity: any) => (
+          {this.state.activities.map((activity) => (
             <List.Item key={activity.id}> {activity.title}</List.Item>
           ))}
 
