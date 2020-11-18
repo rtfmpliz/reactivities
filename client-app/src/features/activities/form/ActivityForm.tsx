b85462a288a15879d761cf26e3a873ee08c3cbe6
wahyu <wahyu@istw.co.id>
2020-11-18 20:17:06 +0700

    6.15 ADd button on Form

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Form, Segment } from 'semantic-ui-react'
+import { Button, Form, Segment } from 'semantic-ui-react'
 
 const ActivityForm = () => {
     return (
-        <Segment>
+        <Segment clearing>
             <Form>
                 <Form.Input placeholder='Title'/>
                 <Form.TextArea rows={2} placeholder='Description'/>
@@ -11,6 +11,8 @@ const ActivityForm = () => {
                 <Form.Input type='date' placeholder='Date'/>
                 <Form.Input placeholder='City'/>
                 <Form.Input placeholder='Venue'/>
+                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button floated='right'  type='button' content='Cancel'/>
             </Form>
         </Segment>
     )
