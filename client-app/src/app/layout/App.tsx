171c16dbb8cac0fa5961aa6ca8eecaf09ada821a
wahyu <wahyu@istw.co.id>
2020-11-23 04:17:17 +0700

    6.18 after create / edit, go to details

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -23,10 +23,14 @@ const App = () => {
 
   const handleCreateActivity = (activity: IActivity) => {
     setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
   }
 
   const handleEditActivity = (activity: IActivity) => {
     setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
   }
   useEffect(() => {
     axios
