351d2a467c4f1da56cc238b7dbef55f78b12f70b
wahyu <wahyu@istw.co.id>
2020-11-29 15:51:07 +0700

    7.3 create

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -29,9 +29,12 @@ const App = () => {
   }
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity])
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+
+    })
   }
 
   const handleDeleteActivity = (id: string) => {
