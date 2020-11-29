7e4c423336d00dd9ed967ecad4d114e049c576ae
wahyu <wahyu@istw.co.id>
2020-11-29 17:10:29 +0700

    7.6 Loading Component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import "./styles.css";
 import { Container } from "semantic-ui-react";
 
@@ -6,72 +6,77 @@ import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleSelectedActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id===id)[0]);
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
     setEditMode(false);
-  }
+  };
 
-  const handleOpenCreateForm =() => {
+  const handleOpenCreateForm = () => {
     setSelectedActivity(null);
     setEditMode(true);
-  }
+  };
 
   const handleCreateActivity = (activity: IActivity) => {
     agent.Activities.create(activity).then(() => {
-
       setActivities([...activities, activity]);
       setSelectedActivity(activity);
       setEditMode(false);
-    })
-  }
+    });
+  };
 
   const handleEditActivity = (activity: IActivity) => {
     agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity])
+      setActivities([
+        ...activities.filter((a) => a.id !== activity.id),
+        activity,
+      ]);
       setSelectedActivity(activity);
       setEditMode(false);
-
-    })
-  }
+    });
+  };
 
   const handleDeleteActivity = (id: string) => {
     agent.Activities.delete(id).then(() => {
-
-      setActivities([...activities.filter(a => a.id !== id)])
-    })
-  }
+      setActivities([...activities.filter((a) => a.id !== id)]);
+    });
+  };
   useEffect(() => {
-    agent.Activities.list()
-    .then((response) => {
-        let activities: IActivity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity)
-        })
-        setActivities(activities);
+    agent.Activities.list().then((response) => {
+      let activities: IActivity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split(".")[0];
+        activities.push(activity);
       });
+      setActivities(activities);
+    }).then(() => setLoading(false));
   }, []);
+
+  if (loading) return <LoadingComponent content='Loading Activities ...'/>
 
   return (
     <Fragment>
-      <NavBar openCreateForm= {handleOpenCreateForm} ></NavBar>
-      <Container style={{marginTop:'7em'}}>
-        <ActivityDashboard 
-        activities={activities} 
-        selectActivity={handleSelectedActivity}
-        selectedActivity={selectedActivity}//selectedActivity={selectedActivity!}
-        editMode={editMode}
-        setEditMode={setEditMode}
-        setSelectedActivity={setSelectedActivity}
-        createActivity={handleCreateActivity}
-        editActivity={handleEditActivity}
-        deleteActivity={handleDeleteActivity}
+      <NavBar openCreateForm={handleOpenCreateForm}></NavBar>
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectedActivity}
+          selectedActivity={selectedActivity} //selectedActivity={selectedActivity!}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
     </Fragment>
