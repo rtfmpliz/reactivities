cd832ff41a42a1ab3bc21c50dbb482a98663770c
wahyu <wahyu@istw.co.id>
2020-11-29 17:12:06 +0700

    7.6 inverted = true

--- client-app/src/app/layout/LoadingComponent.tsx
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Dimmer, Loader } from "semantic-ui-react";
 
 const LoadingComponent: React.FC<{ inverted?: boolean; content?: string }> = ({
-  inverted,
+  inverted = true,
   content,
 }) => {
   return (
