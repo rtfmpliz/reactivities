87460c5bad2241eed163be0615668886d51cfa67
wahyu <wahyu@istw.co.id>
2020-12-16 02:10:06 +0700

    12. Adding routing functionality to Axios

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
+import { history } from '../..';
 import { IActivity } from '../models/activity';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(undefined, error => {
     if (error.response.status === 404) {
-        throw error.response;
+        history.push('/notfound');
     };
 })
 
