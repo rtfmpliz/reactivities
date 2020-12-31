d852af34ebbe99fe8ff336a9ec5c287c1ceece16
wahyu <wahyu@istw.co.id>
2020-12-31 18:10:29 +0700

    14.10 using lazy loading

--- Domain/NCDetail.cs
+++ Domain/NCDetail.cs
@@ -8,6 +8,6 @@ namespace Domain
         public int Line { get; set; }
         public string Partial { get; set; }
 
-        public NC NC { get; set; }
+        public virtual NC NC { get; set; }
     }
 }
