1c0a3f2632cdaf3695ebfcf9d7a6acb30a15096a
wahyu <wahyu@istw.co.id>
2021-01-01 17:55:03 +0700

    10.4 masih error jika ditambah host.displayName

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,6 +5,7 @@ import agent from "../api/agent";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import {RootStore} from './rootStore';
+import { setActivityProps } from "../common/util/util";
 
 configure({ enforceActions: "always" });
 
@@ -39,12 +40,11 @@ export default class ActivityStore {
 
   @action loadActivities = async () => {
     this.loadingInitial = true;
-
     try {
       const activities = await agent.Activities.list();
       runInAction("loading activities", () => {
         activities.forEach((activity) => {
-          activity.date = new Date(activity.date);
+          setActivityProps(activity, this.rootStore.userStore.user!);
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -67,7 +67,7 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         runInAction('get activity', () => {
-          activity.date = new Date(activity.date)
+          setActivityProps(activity, this.rootStore.userStore.user!)
           this.activity = activity;
           this.activityRegistry.set(activity.id, activity);
           this.loadingInitial = false;
--- client-app/src/features/activities/dashboard/ActivityListItems.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItems.tsx
@@ -1,20 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { format } from "date-fns";
 import ActivityListItemAttendees from "./ActivityListItemAttendees";
 
 const ActivityListItems: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  // const host = activity.attendees.filter(x => x.isHost)[0];
+  const host = activity.attendees.filter(x => x.isHost)[0];
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Image
+              size="tiny"
+              circular
+              src={ "/assets/user.png"}
+              />
+              
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Description>Hosted by Bob</Item.Description>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>
+                Hosted by
+                 {host.displayName}
+                 </Item.Description>
+              {activity.isHost && 
+                <Item.Description>
+                  <Label
+                    basic
+                    color="orange"
+                    content="You are hosting this activity"
+                  />
+                </Item.Description>
+              }
+              {activity.isGoing && !activity.isHost && 
+                <Item.Description>
+                  <Label
+                    basic
+                    color="green"
+                    content="You are going to this activity"
+                  />
+                </Item.Description>
+       }
             </Item.Content>
           </Item>
         </Item.Group>
