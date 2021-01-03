7bfb502b500363caad2b0d3935b9558f5fa6cc61
wahyu <wahyu@istw.co.id>
2021-01-03 14:19:20 +0700

    18.13 Adding the Photo methods to store

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { IActivity } from '../models/activity';
-import { IProfile } from '../models/profile';
+import { IPhoto, IProfile } from '../models/profile';
 import {IUser, IUserFormValues} from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
@@ -37,32 +37,41 @@ const responseBody = (response: AxiosResponse) => response.data;
 const sleep = (ms: number) => (response: AxiosResponse) =>
 new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response),ms));
 
-const request = {
+const requests = {
     get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
     post: (url: string, body: {}) => axios.post(url,body).then(sleep(1000)).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
     del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    postForm: (url: string , file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post(url, formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        }).then(responseBody)
+    }
 }
 
 const Activities = {
-    list: (): Promise<IActivity[]> => request.get(`/activities`),
-    details: (id: string) => request.get(`/activities/${id}`),
-    create: (activity: IActivity) => request.post(`/activities`, activity),
-    update: (activity: IActivity) => request.put(`/activities/${activity.id}`,activity),
-    delete: (id: string) => request.del(`/activities/${id}`),
-    attend: (id: string) => request.post(`/activities/${id}/attend`,{}),
-    unattend: (id: string) => request.del(`/activities/${id}/attend`)
+    list: (): Promise<IActivity[]> => requests.get(`/activities`),
+    details: (id: string) => requests.get(`/activities/${id}`),
+    create: (activity: IActivity) => requests.post(`/activities`, activity),
+    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`,activity),
+    delete: (id: string) => requests.del(`/activities/${id}`),
+    attend: (id: string) => requests.post(`/activities/${id}/attend`,{}),
+    unattend: (id: string) => requests.del(`/activities/${id}/attend`)
 }
 
 const User = {
-    current: (): Promise<IUser> => request.get('/user'),
-    login: (user: IUserFormValues): Promise<IUser> => request.post(`/user/login`, user),
-    register: (user: IUserFormValues): Promise<IUser> => request.post(`/user/register`, user), 
+    current: (): Promise<IUser> => requests.get('/user'),
+    login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
+    register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user), 
 
 }
 
 const Profiles = {
-    get: (username: string): Promise<IProfile> => request.get(`/profiles/${username}`)
+    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo)
+
 }
 
 export default {
