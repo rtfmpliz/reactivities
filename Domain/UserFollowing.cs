c73e163857c5352aaff58e33ee2df9fb3ceb77ad
wahyu <wahyu@istw.co.id>
2021-01-09 05:21:46 +0700

    sudah selesai commit

--- Domain/UserFollowing.cs
+++ Domain/UserFollowing.cs
@@ -2,7 +2,7 @@ namespace Domain
 {
     public class UserFollowing
     {
-        public int ObserverId { get; set; }
+        public string ObserverId { get; set; }
         public virtual AppUser Observer { get; set; }
         public string TargetId { get; set; }
         public virtual AppUser Target { get; set; }
