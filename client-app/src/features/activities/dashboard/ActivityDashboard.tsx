52c09122a745bbc38349db4fadaf725aa3e26181
wahyu <wahyu@istw.co.id>
2021-01-16 19:31:32 +0700

    22.2 Swapping indicator for placeholder

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,46 +1,54 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useState } from "react";
-import {  Grid, GridColumn, Loader } from "semantic-ui-react";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { Grid, GridColumn, Loader } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import { RootStoreContext} from '../../../app/stores/rootStore'; 
-import InfiniteScroll from 'react-infinite-scroller'
+import { RootStoreContext } from "../../../app/stores/rootStore";
+import InfiniteScroll from "react-infinite-scroller";
 import ActivityFilters from "./ActivityFilters";
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 const ActivityDashboard: React.FC = () => {
-
   const rootStore = useContext(RootStoreContext);
-  const {loadActivities, loadingInitial, setPage, page, totalPages} = rootStore.activityStore;
+  const {
+    loadActivities,
+    loadingInitial,
+    setPage,
+    page,
+    totalPages,
+  } = rootStore.activityStore;
   const [loadingNext, setLoadingNext] = useState(false);
 
   const handleGetNext = () => {
     setLoadingNext(true);
     setPage(page + 1);
-    loadActivities().then(() => setLoadingNext(false))
-  }
+    loadActivities().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     loadActivities();
   }, [loadActivities]);
 
-  if (loadingInitial && page === 0 )
-    return <LoadingComponent content="Loading Activities ..." />;
-
-
   return (
     <Grid>
       <Grid.Column width={10}>
-        <InfiniteScroll pageStart={0} loadMore={handleGetNext} hasMore={!loadingNext && page + 1 < totalPages} initialLoad={false}>
-
-        <ActivityList />
-        </InfiniteScroll>
-
+        {loadingInitial && page === 0 ? (
+          <ActivityListItemPlaceholder />
+        ) : (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={handleGetNext}
+            hasMore={!loadingNext && page + 1 < totalPages}
+            initialLoad={false}
+          >
+            <ActivityList />
+          </InfiniteScroll>
+        )}
       </Grid.Column>
       <GridColumn width={6}>
-        <ActivityFilters/>
+        <ActivityFilters />
       </GridColumn>
       <GridColumn width={10}>
-        <Loader active={loadingNext}/>
+        <Loader active={loadingNext} />
       </GridColumn>
     </Grid>
   );
